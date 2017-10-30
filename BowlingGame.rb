6e6a6457007b9a5c8e7ec4bc3117df494d38c179
Nik Weinmeister <nw95@vt.edu>
2017-10-30 16:21:49 -0400

    test 3

--- BowlingGame.rb
+++ BowlingGame.rb
@@ -1,13 +1,34 @@
 class BowlingGame
 	def initialize
-		@score = 0
+		@rolls = []
+		@current_roll = 0
+		@frame_index = 0
 	end
 
 	def roll(pins)
-		@score += pins
+		@rolls[@current_roll] = pins
+		@current_roll += 1
 	end
 
 	def score
-		return @score
+		score = 0
+		frame_index = 0
+		for i in 0..9
+			if @rolls[frame_index] + @rolls[frame_index+1] == 10
+				score += 10 + @rolls[frame_index+2]
+				frame_index += 2
+			else
+				score += @rolls[frame_index] + @rolls[frame_index+1]
+				frame_index += 2
+			end
+		end
+		score
+	end
+end
+
+class Frame
+	def initialize(first_roll, second_roll=nil)
+		@first_roll = first_roll
+		@second_roll = second_roll
 	end
 end
--- BowlingGameTest.rb
+++ BowlingGameTest.rb
@@ -16,10 +16,22 @@ class BowlingGameTest < TestCase
 		assert_equal(20, @game.score)
 	end
 
+	def test_roll_one_spare
+		roll_spare
+		@game.roll(3)
+		roll_many(17, 0)
+		assert_equal(16, @game.score)
+	end
+
 	def roll_many(rolls, pins)
 		for i in 1..rolls
 			@game.roll(pins)
 		end
+	end
+
+	def roll_spare
+		@game.roll(5)
+		@game.roll(5)
 	end
 end
 
